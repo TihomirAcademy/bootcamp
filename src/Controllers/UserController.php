d062a39762bf8ab7507f4afda948840294db8762
GAMMA-TEACHER\Teacher <tihomir@advanceacademy.bg>
2022-01-20 19:51:55 +0200

    - removed method added echo

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -7,16 +7,12 @@ class UserController
 
     public function __construct()
     {
-       
+       echo "asddasdsa";
     }
 
     public function getUserList()
     {
         return 'hello';
-    }
-    public function getCarList()
-    {
-        return 'car list';
     }
 
     public function getUser(integer $id)
